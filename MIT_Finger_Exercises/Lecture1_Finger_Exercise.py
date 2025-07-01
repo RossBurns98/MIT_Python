19c035679d9a44a31f4ea9781934e1bcb37a04be
RossBurns98 <196393235+RossBurns98@users.noreply.github.com>
2025-07-01 02:37:41 +0100

    Added values so the code ran

--- MIT_Finger_Exercises/Lecture1_Finger_Exercise.py
+++ MIT_Finger_Exercises/Lecture1_Finger_Exercise.py
@@ -3,6 +3,9 @@
 # Create a variable called total that adds a and b then multiplies the result by c.
 # Include a last line in your code to print the value: print(total)
 
-#assuming a, b and c are all assigned values already
+#assigned values for a, b and c just for clarity/ making the program run.
+a = 1
+b = 2
+c = 3
 total = (a + b)*c
 print(total)
