e9669404b10c0a7f4330d6de58421ecb591fe4df
RossBurns98 <196393235+RossBurns98@users.noreply.github.com>
2025-07-01 02:36:10 +0100

    FInger Exercise 1

---
+++ MIT_Finger_Exercises/Lecture1_Finger_Exercise.py
@@ -0,0 +1,8 @@
+# Finger Exercise Prompt: 
+# Assume 3 variables are already defined for you: a, b, and c. 
+# Create a variable called total that adds a and b then multiplies the result by c.
+# Include a last line in your code to print the value: print(total)
+
+#assuming a, b and c are all assigned values already
+total = (a + b)*c
+print(total)
